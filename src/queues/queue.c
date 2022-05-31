449300e8371b7af11cbd0fa5be32875b055a6db7
dvdmakeev <dvdmakeev@gmail.com>
2022-05-31 22:28:15 +0300

    queue interface added

---
+++ src/queues/queue.c
@@ -0,0 +1,26 @@
+#include "queue.h"
+
+queue_t* create_queue()
+{
+    
+}
+
+void delete_queue(queue_t* queue)
+{
+
+}
+
+void enqueue(queue_t* queue, int val)
+{
+
+}
+
+int dequeue(queue_t* queue)
+{
+
+}
+
+int is_empty(queue_t* queue)
+{
+
+}
